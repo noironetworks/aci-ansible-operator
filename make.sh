4ab1bc860eb6d9b6d0a4b560ab7a26958ad02b6f
Kent Wu <wu@noironetworks.com>
2020-09-21 16:39:19 -0400

    Add the support for the epgs.aci.aw CRD
    1. added the CRD and CR yaml file definition.
    2. updated those deployment yaml files including service_account.yaml,
    role.yaml, role_binding.yaml & operator.yaml.
    3. updated the docker build file.
    4. updated the watches.yaml file.
    5. created the epg-playbook.yaml file.
    6. created a useful script make.sh to build and deploy the whole thing.

---
+++ make.sh
@@ -0,0 +1,10 @@
+kubectl delete -f deploy/operator.yaml
+sudo ./operator-sdk build kentwu111/aci-ansible-operator:v1
+sudo docker push kentwu111/aci-ansible-operator:v1
+kubectl apply -f deploy/service_account.yaml
+kubectl apply -f deploy/role.yaml
+kubectl apply -f deploy/role_binding.yaml
+kubectl apply -f deploy/operator.yaml
+kubectl get pods -n aci-containers-system
+kubectl logs -n=aci-containers-system aci-operator-7d55d4465f-g5r8r
+
